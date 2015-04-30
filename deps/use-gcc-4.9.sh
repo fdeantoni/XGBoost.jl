cfcb3ae9d22940fee0ac2b0cb7394a2d6def8ebc
fdeantoni <fdeantoni@farmington.com.hk>
2015-04-30 10:03:28 +0800

    Julia overrides gcc despite what is defined in the makefile

--- deps/use-gcc-4.9.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# In OSX we want to use the homebrew GCC to support OpenMP
-if [[ $OSTYPE == darwin* ]] && [ -f /usr/local/bin/gcc-4.9 ]; then	
-	sed -i '.bak' "1s/export CC.*/export CC = \/usr\/local\/bin\/gcc-4.9/" Makefile
-	sed -i '.bak' "2s/export CXX.*/export CCX = \/usr\/local\/bin\/g++-4.9/" Makefile
-fi
